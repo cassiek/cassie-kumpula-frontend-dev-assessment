3978cd1e501d66b2bf9ef597f6eedd54726286f8
cassiek <ckumpula123@gmail.com>
2025-09-29 10:52:09 -0400

    troubleshoot ScatterChart rendering in Chart component

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -7,15 +7,17 @@ function Chart() {
     const csvData = useContext(DataContext);
 
     return (
-        <ResponsiveContainer width="40rem" height="40rem" className="chart">
-            <ScatterChart width={300} height={300}>
-                <CartesianGrid />
-                <XAxis dataKey="time" type="number" name="Time" unit="" />
-                <YAxis dataKey="mag" type="number" name="Magnitude of Event" unit="" />
-                <Legend />
-                <Scatter name="Earthquake Event Data" data={csvData} fill="#8884d8" />
-            </ScatterChart>
-        </ResponsiveContainer>
+        <section className="chart">
+            <ResponsiveContainer width="100%" height="100%" className="chart-container">
+                <ScatterChart>
+                    <CartesianGrid />
+                    <XAxis dataKey="time" type="number" name="Time" unit="" />
+                    <YAxis dataKey="mag" type="number" name="Magnitude of Event" unit="" />
+                    <Legend />
+                    <Scatter name="Earthquake Event Data" data={csvData} fill="#8884d8" />
+                </ScatterChart>
+            </ResponsiveContainer>
+        </section>
     )
 }
 
