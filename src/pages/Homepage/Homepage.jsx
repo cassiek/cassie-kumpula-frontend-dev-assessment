76c61985049dfcfbccd4d2da3df0ec77039f8a2f
cassiek <ckumpula123@gmail.com>
2025-09-28 22:57:24 -0400

    import Chart to Homepage

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,11 +1,12 @@
 import CSVDisplay from "../../components/CSVDisplay/CSVDisplay";
+import Chart from "../../components/Chart/Chart";
 
 function Homepage() {
 
 
     return (
         <main>
-
+            <Chart />
             <CSVDisplay />
         </main>
     )
