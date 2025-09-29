c01f0d9a7d81e35339f1997d6acc40a763e64363
cassiek <ckumpula123@gmail.com>
2025-09-28 23:12:45 -0400

    give Chart component some styling

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,12 +1,12 @@
-import { ResponsiveContainer, ScatterChart, CartesianGrid, XAxis, YAxis, Legend, CartesianGrid } from "recharts";
-import "Chart.scss";
+import { ResponsiveContainer, ScatterChart, CartesianGrid, XAxis, YAxis, Legend } from "recharts";
+import "./Chart.scss";
 const data = [{}];
 
 function Chart() {
 
     return (
-        <ResponsiveContainer width="30rem" height="40rem">
-            <ScatterChart data={data} className="chart">
+        <ResponsiveContainer width="30rem" height="40rem" className="chart">
+            <ScatterChart data={data}>
                 <CartesianGrid />
                 <XAxis />
                 <YAxis />
