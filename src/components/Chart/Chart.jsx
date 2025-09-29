0713c51f31c0934f26a34c3ee163b1eacfa7986a
cassiek <ckumpula123@gmail.com>
2025-09-29 01:16:01 -0400

    work on Chart implemmentation

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,16 +1,19 @@
-import { ResponsiveContainer, ScatterChart, CartesianGrid, XAxis, YAxis, Legend } from "recharts";
+import { ResponsiveContainer, ScatterChart, CartesianGrid, XAxis, YAxis, Legend, Scatter } from "recharts";
 import "./Chart.scss";
+import { useContext } from "react";
+import { DataContext } from "../../pages/Homepage/Homepage";
 
-
-function Chart({ data }) {
+function Chart() {
+    const csvData = useContext(DataContext);
 
     return (
         <ResponsiveContainer width="40rem" height="40rem" className="chart">
-            <ScatterChart data={data}>
+            <ScatterChart width={300} height={300}>
                 <CartesianGrid />
-                <XAxis />
-                <YAxis />
+                <XAxis dataKey="time" type="number" name="Time" unit="" />
+                <YAxis dataKey="mag" type="number" name="Magnitude of Event" unit="" />
                 <Legend />
+                <Scatter name="Earthquake Event Data" data={csvData} fill="#8884d8" />
             </ScatterChart>
         </ResponsiveContainer>
     )
